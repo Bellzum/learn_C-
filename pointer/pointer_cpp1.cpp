aec2939a9e4e1a27268bcd2b524fb45b8a479697
Umbhorn Ungkulpasvich <umbhorn.ung@gmail.com>
2025-10-12 17:28:58 +0900

    new exercise cpp code

---
+++ pointer/pointer_cpp1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main() 
+{
+    int a = 54;
+    int b = 21;
+    std::cout << "a = " << a << " address: " << &a << "\n";
+    std::cout << "b = " << b << " address: " << &b << "\n";
+}
